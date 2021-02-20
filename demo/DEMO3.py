8b97b363657126ecd3e52402bcdfbaa7fc825bda
u0075u006b <sojuuk@sohu.com>
2021-02-20 16:51:00 +0800

    add listmenu

--- demo/DEMO3.py
+++ demo/DEMO3.py
@@ -12,16 +12,17 @@ class ItemWidget(QWidget):
         self.item = item
 
         self.lay = QFormLayout(self)
-        self.lay.setContentsMargins(2,0,0,0)
+        self.lay.setContentsMargins(0,5,0,5)
         self.lay.setSpacing(0)
+        self.lay.setVerticalSpacing(3)
         self.bt1 = QPushButton("A")
-        self.bt1.setFixedHeight(18)
+        self.bt1.setFixedHeight(20)
         self.lay.addWidget(self.bt1)
-        print(self.lay.sizeHint())
+        print(self.lay.sizeHint())#
         self.bt2 = QPushButton("B")
-        self.bt2.setFixedHeight(18)
+        self.bt2.setFixedHeight(20)
         self.lay.addWidget(self.bt2)
-        print(self.lay.sizeHint())
+        print(self.lay.sizeHint())#
 
 
 
@@ -37,9 +38,20 @@ class ItemWidget(QWidget):
     #     txt = self.sender().text()  # 获取发送信号的控件文本
     #     self.update_.emit(txt)
 
+
 class TopButton(QPushButton):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, item, name):
+        super(TopButton, self).__init__()
+        self.item = item
+        self.setCheckable(True) # 设置可选中
+        self.setChecked(True)
+        self.setText(name)
+        self.setFixedHeight(22)
+
+    def resizeEvent(self, event):
+        # 解决item的高度问题
+        super(TopButton, self).resizeEvent(event)
+        self.item.setSizeHint(QSize(self.minimumWidth(), self.height()))
 
 class LeftItem(QListWidget):
     def __init__(self,btname,listname):
@@ -48,27 +60,24 @@ class LeftItem(QListWidget):
         self.itemlist = listname
 
         self.top = QListWidgetItem(self)
-        self.btn = QPushButton(self.btname)
-        self.btn.setFixedHeight(23)
+        self.btn = TopButton(self.top,self.btname)
         self.setItemWidget(self.top,self.btn)
 
         self.sub_item = QListWidgetItem(self)
+        self.btn.toggled.connect(self.sub_item.setHidden)
         self.f = ItemWidget(self.sub_item, self.itemlist)
         self.setItemWidget(self.sub_item,self.f)
+        self.sub_item.setHidden(True)
 
         self.top = QListWidgetItem(self)
-        self.btn = QPushButton(self.btname)
-        self.btn.setFixedHeight(23)
+        self.btn = TopButton(self.top,self.btname)
         self.setItemWidget(self.top,self.btn)
 
         self.sub_item = QListWidgetItem(self)
         self.f = ItemWidget(self.sub_item, self.itemlist)
         self.setItemWidget(self.sub_item,self.f)
 
-    def resizeEvent(self, event):
-        # 解决item的高度问题
-        super(LeftItem, self).resizeEvent(event)
-        self.setSizeHint(QSize(self.minimumWidth(), self.height()))
+
 
 
 
