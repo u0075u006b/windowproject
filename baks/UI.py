12ab2ecf2a162d703ff955cfa63a641c7e6c7ac6
u0075u006b <sojuuk@sohu.com>
2021-03-31 13:48:51 +0800

    202103031

---
+++ baks/UI.py
@@ -0,0 +1,19 @@
+"""
+vtoolmenu.py create object loop
+"""
+# for _i in range(len(self.btname)):
+# top_obj = QListWidgetItem(self)
+# self.btn = TopButton(top_obj, self.btname[_i],self.t_icon_fold,self.t_icon_unfold)
+# self.btn.setObjectName("menu_0")
+# # self.btn.setIcon(self.t_icon_fold)
+# self.btn.setFixedHeight(self.__btheight)
+# self.btn.setStyleSheet(self.__qss)
+# self.setItemWidget(top_obj, self.btn)
+# if self.itemlist[_i]:
+#     sub_obj = QListWidgetItem(self)
+#     self.btn.toggled.connect(sub_obj.setHidden)
+#     self.itemc = ItemWidget(sub_obj, self.itemlist[_i],self.__qss,self.__itemper,self.__btheight)
+#     self.setItemWidget(sub_obj, self.itemc)
+#     sub_obj.setHidden(True)
+# else:
+#     pass
